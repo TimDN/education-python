765af33af24da380a1847adc2d263d6782182c8b
Tim Nielsen <tim.nielsen@codic.se>
2020-09-01 21:11:15 +0200

    Update ternary

---
+++ conditional/ternary.py
@@ -0,0 +1,12 @@
+def ternary_def():
+    result = <expr1> if <condition> else <expr2>
+
+
+def ternary_example():
+    a = 5
+    b = 6
+    max_value = a if a > b else b
+    print(max_value) #prints 6
+
+
+ternary_example()
